338e5214a04171891f8a72ca6c04dd32b92ea881
YunJehyeok <dbswpgur2@naver.com>
2021-07-10 19:04:18 +0900

    7/10 Settings - Yunjehyeok

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Header } from "./styles";
 
 const Login = () => {
-  return <div>Login</div>;
+  return <Header>aa</Header>;
 };
 
 export default Login;
--- client/src/pages/Login/styles.tsx
+++ client/src/pages/Login/styles.tsx
@@ -1,1 +1,5 @@
 import styled from "styled-components";
+
+export const Header = styled.div`
+  font-size: 48px;
+`;
