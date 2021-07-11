8f7721d67fd6b192c1b5e8198f4619b482b36882
YunJehyeok <dbswpgur2@naver.com>
2021-07-11 10:27:52 +0900

    ok

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,15 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-import { Link } from "react-router-dom";
-
-const Main = () => {
-  return (
-    <div>
-      <Link to="/login">로그인</Link>
-    </div>
-  );
-=======
-import Header from '../../components/navbar';
+import Header from "../../components/navbar";
 import History from "../../components/history";
 import styled from "styled-components";
 
@@ -25,8 +15,7 @@ const Main = () => {
         <History />
       </Body>
     </>
-  )
->>>>>>> bb88ae61156203aa886cd16d66caecb578ed6c72
+  );
 };
 
 export default Main;
