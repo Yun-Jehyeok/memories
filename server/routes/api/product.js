7df6ab4be188b891ee9a26828137c5ffa617e718
Seulgi_LEE <dl4459@naver.com>
2021-08-07 16:20:44 +0900

    8/7 Like server setting -seulgi

--- server/routes/api/product.js
+++ server/routes/api/product.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const { Product } = require("../../models/product");
-const multer = require("multer");
+const express = require('express');
+const { Product } = require('../../models/product');
+const { Like } = require('../../models/Like');
+const multer = require('multer');
 
 const router = express.Router();
 
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}_${file.originalname}`);
@@ -14,29 +15,29 @@ var storage = multer.diskStorage({
   fileFilter: (req, file, cb) => {
     const ext = path.extname(file.originalname);
 
-    if (ext !== ".jpg" || ext !== ".png") {
-      return cb(res.status(400).end("only jpg, png are allowed"), false);
+    if (ext !== '.jpg' || ext !== '.png') {
+      return cb(res.status(400).end('only jpg, png are allowed'), false);
     }
 
     cb(null, true);
   },
 });
 
-var upload = multer({ storage: storage }).single("file");
+var upload = multer({ storage: storage }).single('file');
 
 ///////////////////////////////////////////////
 //              Product Router
 ///////////////////////////////////////////////
 
 // Get Products
-router.get("/getProducts", async (req, res) => {
+router.get('/getProducts', async (req, res) => {
   try {
     const products = await Product.find();
 
     if (!products)
       return res
         .status(400)
-        .json({ success: false, msg: "상품이 존재하지 않습니다." });
+        .json({ success: false, msg: '상품이 존재하지 않습니다.' });
 
     res.status(200).json({ success: true, products });
   } catch (e) {
@@ -46,7 +47,7 @@ router.get("/getProducts", async (req, res) => {
 
 // Upload Image
 // 서버의 multer library에 이미지 저장
-router.post("/uploadImage", (req, res) => {
+router.post('/uploadImage', (req, res) => {
   upload(req, res, (err) => {
     if (err) return res.json({ success: false, err });
 
@@ -60,7 +61,7 @@ router.post("/uploadImage", (req, res) => {
 
 // Upload Product
 // POST
-router.post("/uploadProduct", (req, res) => {
+router.post('/uploadProduct', (req, res) => {
   console.log(req.body);
 
   const product = new Product(req.body);
@@ -77,15 +78,46 @@ router.post("/uploadProduct", (req, res) => {
 
 // Get Product By Id
 // GET
-router.get("/products_by_id", async (req, res) => {
+router.get('/products_by_id', async (req, res) => {
   try {
-    const product = await Product.findById(req.query.id).populate("writer");
+    const product = await Product.findById(req.query.id).populate('writer');
 
     console.log(product);
 
     return res.status(200).send(product.images);
   } catch (e) {
     console.error(e);
+  }
+});
+
+///////////////////////////////////////////////
+//                   Like
+///////////////////////////////////////////////
+
+router.post('/:id/like/uplike', async (req, res) => {
+  try {
+    let { productId, userId } = req.body;
+    const newLike = await Like.create({
+      userId,
+      productId,
+    });
+    return res.status(200).json({ upLike: true });
+  } catch (e) {
+    console.error(e);
+    return res.status(400).json({ upLike: false, e });
+  }
+});
+
+router.post('/:id/like/unlike', async (req, res) => {
+  try {
+    Like.findOneAndDelete({
+      userId: req.body.userId,
+      productId: req.body.productId,
+    });
+    return res.status(200).json({ unLike: true });
+  } catch (e) {
+    console.error(e);
+    return res.status(400).json({ unLike: false, e });
   }
 });
 
