d62a69d577417fc6b01be2d0e746e8af7d44026d
dae <h33223012@naver.com>
2021-08-07 15:33:27 +0900

    update0807

--- client/src/components/exhibition/index.js
+++ client/src/components/exhibition/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Page } from './styles';
+import { Page, Text } from './styles';
 
 const Exhibition = () => {
   return (
     <>
       <Page className="page_section" id="exhibition_area">
-        <div></div>
+        <Text
+          className="content_main"
+          style={{
+            position: 'absolute',
+            width: '417px',
+            height: '84px',
+            left: '608px',
+            top: '338px',
+
+            color: 'rgba(255, 255, 255, 0.6)',
+          }}
+        >
+          HIdfsffsfsff
+        </Text>
       </Page>
     </>
   );
--- client/src/components/exhibition/styles.js
+++ client/src/components/exhibition/styles.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
 
 export const Page = styled.div`
-  background: red;
+  background: #d0d096;
   height: 100vh;
   width: 100%;
+`;
+export const Text = styled.div`
+  font-family: 'Roboto';
+  font-style: 'normal';
+  font-weight: 'normal';
+  font-size: '72px';
+  line-height: '84px';
+  letter-spacing: '0.05em';
 `;
