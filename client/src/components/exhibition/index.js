048dd34f3c12f689bc9d6bb6eb7d48ff20542e2a
dae <h33223012@naver.com>
2021-08-08 14:40:11 +0900

    update0808

--- client/src/components/exhibition/index.js
+++ client/src/components/exhibition/index.js
@@ -1,23 +1,76 @@
 import React from 'react';
-import { Page, Text } from './styles';
+import { Page, Text, Box } from './styles';
 
 const Exhibition = () => {
   return (
     <>
       <Page className="page_section" id="exhibition_area">
-        <Text
-          className="content_main"
-          style={{
-            position: 'absolute',
-            width: '417px',
-            height: '84px',
-            left: '608px',
-            top: '338px',
+        <Text className="content_main" style={{ margin: '0px' }}>
+          <div>
+            <center>EXHIBITION</center>
+          </div>
 
-            color: 'rgba(255, 255, 255, 0.6)',
-          }}
-        >
-          HIdfsffsfsff
+          <center_abs>
+            <div
+              className="abstract"
+              style={{
+                position: 'absolute',
+                paddingLeft: '510px',
+                paddingTop: '50px',
+              }}
+            >
+              <>
+                <div>Where</div>
+                <div>Do</div>
+                <div>You</div>
+                Go?
+              </>
+            </div>
+          </center_abs>
+
+          <Box
+            className="length"
+            style={{
+              left: '12%',
+
+              width: '300px',
+              height: '400px',
+            }}
+          ></Box>
+          <Box
+            className="length"
+            style={{
+              left: '53%',
+
+              width: '300px',
+              height: '400px',
+            }}
+          ></Box>
+
+          <Box
+            className="width"
+            style={{
+              left: '10%',
+              bottom: '55%',
+              width: '400px',
+              height: '300px',
+            }}
+          ></Box>
+          <ex_abs>
+            <div
+              className="ex_abs"
+              style={{
+                paddingLeft: '1200px',
+                paddingTop: '250px',
+              }}
+            >
+              국가를 위해
+              <br />
+              헌신한 분들을 위한
+              <br />
+              전시회
+            </div>
+          </ex_abs>
         </Text>
       </Page>
     </>
